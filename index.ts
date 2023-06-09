14ab54fd621236ab51e4208b06f290a6e09f3f4f
Amit-kharod <kharodcloud1@gmail.com>
2023-06-09 18:47:24 +0530

    mend

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
